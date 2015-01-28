fb66b7a107a110a6486e7541e718ca0e59a08296
罗宝龙 <luobaolong@gmail.com>
2015-01-28 16:57:07 +0800

    TF界面动态加载（有问题）

--- src/com/chipsguide/app/colorbluetoothlamp/v2/frags/MusicFrag.java
+++ src/com/chipsguide/app/colorbluetoothlamp/v2/frags/MusicFrag.java
@@ -4,36 +4,47 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.bluetooth.BluetoothDevice;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import com.chipsguide.app.colorbluetoothlamp.v2.R;
+import com.chipsguide.app.colorbluetoothlamp.v2.bluetooth.BluetoothDeviceManagerProxy;
 import com.chipsguide.app.colorbluetoothlamp.v2.listener.SimpleMusicPlayListener;
 import com.chipsguide.app.colorbluetoothlamp.v2.media.PlayListener;
 import com.chipsguide.app.colorbluetoothlamp.v2.media.PlayerManager;
 import com.chipsguide.app.colorbluetoothlamp.v2.media.PlayerManager.PlayType;
+import com.chipsguide.lib.bluetooth.interfaces.callbacks.OnBluetoothDeviceConnectionStateChangedListener;
+import com.chipsguide.lib.bluetooth.managers.BluetoothDeviceManager;
 
-public class MusicFrag extends BaseFragment implements OnPageChangeListener , OnCheckedChangeListener{
+public class MusicFrag extends BaseFragment implements OnPageChangeListener,
+		OnCheckedChangeListener,
+		OnBluetoothDeviceConnectionStateChangedListener {
 	private ViewPager viewPager;
 	private RadioGroup topNavRg;
 	private PlayerManager manager;
 	private boolean hasUpdate;
 	private MyPlayListener playListener;
-	
-	private List<Fragment> fragments = new ArrayList<Fragment>();
-	
+	private BluetoothDeviceManagerProxy btDeviceManProxy;
+
+	private MyPagerAdapter adapter;
+	private boolean connected;
+
 	@Override
 	protected void initBase() {
+		btDeviceManProxy = BluetoothDeviceManagerProxy
+				.getInstance(getActivity().getApplicationContext());
+		btDeviceManProxy
+				.addOnBluetoothDeviceConnectionStateChangedListener(this);
 		playListener = new MyPlayListener(this);
-		manager = PlayerManager.getInstance(getActivity().getApplicationContext());
-		fragments.add(new MyMusicFrag());
-		fragments.add(new TFCardMusicFrag());
-		fragments.add(new CloudMusicFrag());
+		manager = PlayerManager.getInstance(getActivity()
+				.getApplicationContext());
 	}
 
 	@Override
@@ -43,9 +54,16 @@ public class MusicFrag extends BaseFragment implements OnPageChangeListener , On
 
 	@Override
 	protected void initView() {
+		List<Fragment> fragments = new ArrayList<Fragment>();
+		fragments.add(new MyMusicFrag());
+		fragments.add(new CloudMusicFrag());
+		adapter = new MyPagerAdapter(getChildFragmentManager());
+		adapter.setList(fragments);
+		findViewById(R.id.rb_tf_card_music).setVisibility(View.GONE);
+
 		viewPager = (ViewPager) findViewById(R.id.viewPager);
 		viewPager.setOffscreenPageLimit(2);
-		viewPager.setAdapter(new MyPagerAdapter(getChildFragmentManager()));
+		viewPager.setAdapter(adapter);
 		viewPager.setOnPageChangeListener(this);
 		topNavRg = (RadioGroup) findViewById(R.id.rg_top_nav);
 		topNavRg.setOnCheckedChangeListener(this);
@@ -56,10 +74,22 @@ public class MusicFrag extends BaseFragment implements OnPageChangeListener , On
 	protected void initData() {
 	}
 
-	private class MyPagerAdapter extends FragmentPagerAdapter{
-
+	private class MyPagerAdapter extends FragmentPagerAdapter {
+		private List<Fragment> fragments = new ArrayList<Fragment>();
 		public MyPagerAdapter(FragmentManager fm) {
 			super(fm);
+		}
+
+		public void setList(List<Fragment> list) {
+			if (list == null) {
+				return;
+			}
+			fragments = list;
+			notifyDataSetChanged();
+		}
+
+		public List<Fragment> getFragments() {
+			return fragments;
 		}
 
 		@Override
@@ -71,94 +101,96 @@ public class MusicFrag extends BaseFragment implements OnPageChangeListener , On
 		public int getCount() {
 			return fragments.size();
 		}
+		
 	}
-	
-	public void updateUI(boolean isPlaying){
-		if(manager == null){
+
+	public void updateUI(boolean isPlaying) {
+		if (manager == null) {
 			return;
 		}
 		onMusicChange(null, null);
 	}
 
-	private static class MyPlayListener extends PlayListener{
+	private static class MyPlayListener extends PlayListener {
 		private WeakReference<MusicFrag> ref;
-		private MyPlayListener(MusicFrag frag){
+
+		private MyPlayListener(MusicFrag frag) {
 			ref = new WeakReference<MusicFrag>(frag);
 		}
 
 		@Override
 		public void onMusicStart(String musicSymbol) {
 			MusicFrag frag = ref.get();
-			if(frag != null){
+			if (frag != null) {
 				frag.updateUI(true);
 			}
 		}
-		
+
 		@Override
 		public void onMusicProgress(String musicSymbol, long duration,
 				long currentDuration, int percent) {
 			MusicFrag frag = ref.get();
-			if(frag != null){
-				if(!frag.hasUpdate){
+			if (frag != null) {
+				if (!frag.hasUpdate) {
 					frag.hasUpdate = true;
 					frag.updateUI(frag.manager.isPlaying());
 				}
 			}
 		}
-		
+
 		@Override
 		public void onMusicPause(String musicSymbol) {
 			MusicFrag frag = ref.get();
-			if(frag != null){
+			if (frag != null) {
 				frag.updateUI(false);
 			}
 		}
-		
+
 		@Override
-		public void onMusicError(String musicSymbol, int what ,int extra) {
+		public void onMusicError(String musicSymbol, int what, int extra) {
 		}
-		
+
 		@Override
 		public void onMusicChange(String musicSymbol) {
 		}
-		
+
 		@Override
 		public void onMusicBuffering(String musicSymbol, int percent) {
 		}
-		
+
 		@Override
 		public void onPlayTypeChange(PlayType oldType, PlayType newType) {
 			MusicFrag frag = ref.get();
-			if(frag != null){
+			if (frag != null) {
 				frag.onMusicChange(oldType, newType);
 			}
 		}
 	}
-	
+
 	private void onMusicChange(PlayType oldType, PlayType newType) {
-		int size = fragments.size();
-		for(int i = 0 ; i < size ; i++){
-			Fragment frag = fragments.get(i);
-			if(frag instanceof SimpleMusicPlayListener){
+		int size = adapter.getFragments().size();
+		for (int i = 0; i < size; i++) {
+			Fragment frag = adapter.getFragments().get(i);
+			if (frag instanceof SimpleMusicPlayListener) {
 				SimpleMusicPlayListener listener = (SimpleMusicPlayListener) frag;
 				listener.onMusicChange();
 			}
 		}
 	}
-	
+
 	@Override
 	public void onPause() {
 		super.onPause();
 		hasUpdate = false;
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
-		//调转到播放界面会使监听失效，所以要在onResume()中重新设置
+		// 调转到播放界面会使监听失效，所以要在onResume()中重新设置
 		manager.setPlayListener(playListener, null, true);
 	}
-	
+
 	@Override
 	public void onPageScrollStateChanged(int arg0) {
 	}
@@ -175,6 +207,9 @@ public class MusicFrag extends BaseFragment implements OnPageChangeListener , On
 			break;
 		case 1:
 			checkedId = R.id.rb_tf_card_music;
+			if (!connected) {
+				checkedId = R.id.rb_cloud_music;
+			}
 			break;
 		case 2:
 			checkedId = R.id.rb_cloud_music;
@@ -196,11 +231,45 @@ public class MusicFrag extends BaseFragment implements OnPageChangeListener , On
 			break;
 		case R.id.rb_cloud_music:
 			item = 2;
+			if (!connected) {
+				item = 1;
+			}
 			break;
 		default:
 			break;
 		}
 		viewPager.setCurrentItem(item);
 	}
-	
+
+	@Override
+	public void onBluetoothDeviceConnectionStateChanged(BluetoothDevice device,
+			int state) {
+		List<Fragment> fragments = new ArrayList<Fragment>();
+		switch (state) {
+		case BluetoothDeviceManager.ConnectionState.CONNECTED:
+			connected = true;
+			findViewById(R.id.rb_tf_card_music).setVisibility(View.VISIBLE);
+			fragments.add(new MyMusicFrag());
+			fragments.add(new TFCardMusicFrag());
+			fragments.add(new CloudMusicFrag());
+			adapter.setList(fragments);
+			break;
+		case BluetoothDeviceManager.ConnectionState.DISCONNECTED:
+		case BluetoothDeviceManager.ConnectionState.SPP_DISCONNECTED:
+			connected = false;
+			findViewById(R.id.rb_tf_card_music).setVisibility(View.GONE);
+			fragments.add(new MyMusicFrag());
+			fragments.add(new CloudMusicFrag());
+			adapter.setList(fragments);
+			break;
+		}
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		btDeviceManProxy
+				.removeOnBluetoothDeviceConnectionStateChangedListener(this);
+	}
+
 }
