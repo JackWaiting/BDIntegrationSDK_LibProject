924cea48af8d9b13de8508c84024ae123de4ac9d
fiskz <root@fiskz.com>
2015-02-10 15:32:40 +0800

    点击睡眠隐藏选择时间

--- src/com/chipsguide/app/colorbluetoothlamp/v2/activity/SleepAssistantActivity.java
+++ src/com/chipsguide/app/colorbluetoothlamp/v2/activity/SleepAssistantActivity.java
@@ -1,9 +1,13 @@
 package com.chipsguide.app.colorbluetoothlamp.v2.activity;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.media.AudioManager;
 import android.os.CountDownTimer;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -11,6 +15,7 @@ import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
 import com.chipsguide.app.colorbluetoothlamp.v2.R;
+import com.chipsguide.app.colorbluetoothlamp.v2.media.PlayerManager;
 import com.chipsguide.app.colorbluetoothlamp.v2.utils.FormatHelper;
 import com.chipsguide.app.colorbluetoothlamp.v2.view.TitleView;
 
@@ -28,14 +33,20 @@ public class SleepAssistantActivity extends BaseActivity implements
 	private RadioButton mButton90ReadioButton;
 	private TitleView mSleepTitleview;
 
+	private LinearLayout mVisibilityProgressbarLinearLayout;
+	private LinearLayout mVisibilitySelectorTimeLinearLayout;
+
+	private PlayerManager playerManager;
 	private AudioManager mAudioManager;
-	private int current;//当前音量
-	private int max;//最大音量
+	private int current;// 当前音量
+	private int max;// 最大音量
 	private MyCount mCount;
 	private int mColorTextDown;
 	private int mColorTextNor;
 	private int time = 10;
-	private int TIME_GAP = 6;//分6段减小音量
+	private int TIME_GAP = 6;// 分6段减小音量
+
+	private boolean mSleepMode = false;
 
 	@Override
 	public int getLayoutId()
@@ -48,17 +59,19 @@ public class SleepAssistantActivity extends BaseActivity implements
 	{
 		mColorTextDown = getResources().getColor(R.color.color_blue);
 		mColorTextNor = getResources().getColor(R.color.white);
-		mAudioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 		max = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 		current = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+		playerManager = PlayerManager.getInstance(getApplicationContext());
 	}
 
 	@Override
 	public void initUI()
 	{
-		mSleepTitleview = (TitleView)this.findViewById(R.id.titleview_sleep);
+		mSleepTitleview = (TitleView) this.findViewById(R.id.titleview_sleep);
 		mShowTimeTextView = (TextView) this
 				.findViewById(R.id.textview_show_time);
+		mShowTimeTextView.setTypeface(Typeface.createFromAsset(getAssets(),"font/digifaw.ttf"));
 		mProgressBar = (ProgressBar) this.findViewById(R.id.progressBar);
 		mConfirmationTextView = (TextView) this
 				.findViewById(R.id.textview_confirmation);
@@ -75,8 +88,12 @@ public class SleepAssistantActivity extends BaseActivity implements
 		mButton90ReadioButton = (RadioButton) this
 				.findViewById(R.id.readioButton_button_90);
 
+		mVisibilityProgressbarLinearLayout = (LinearLayout) this
+				.findViewById(R.id.linearlayout_visibility_progressbar);
+		mVisibilitySelectorTimeLinearLayout = (LinearLayout) this
+				.findViewById(R.id.linearlayout_visibility_selector_time);
+
 		mSleepTimeRadiogroup.setOnCheckedChangeListener(this);
-		// mProgressBar.seton
 		mConfirmationTextView.setOnClickListener(this);
 	}
 
@@ -98,19 +115,46 @@ public class SleepAssistantActivity extends BaseActivity implements
 		switch (v.getId())
 		{
 		case R.id.textview_confirmation:
-			mSleepTitleview.setLiftBtnVisibility(false);
-			
-			if (mCount != null)
+			if (mSleepMode)
 			{
-				mCount.cancel();
-				mCount = null;
+				cancelSleep();
+				mSleepMode = false;
+			} else
+			{
+				mConfirmationTextView.setText(getResources().getString(
+						R.string.confirmation_cancel));
+				mSleepTitleview.setLiftBtnVisibility(false);
+				mVisibilityProgressbarLinearLayout.setVisibility(View.VISIBLE);
+				mVisibilitySelectorTimeLinearLayout
+						.setVisibility(View.INVISIBLE);
+				if (mCount != null)
+				{
+					mCount.cancel();
+					mCount = null;
+				}
+				mCount = new MyCount(60000 * time, 1000);
+				mShowTimeTextView.setText(FormatHelper
+						.formatLongToTimeMinuteStr((long) time * 60000));
+				mCount.start();
+				mSleepMode = true;
 			}
-			mCount = new MyCount(60000 * time, 1000);
-			mShowTimeTextView.setText(FormatHelper
-					.formatLongToTimeMinuteStr((long) time * 60000));
-			mCount.start();
 			break;
 		}
+	}
+
+	private void cancelSleep()
+	{
+		mConfirmationTextView.setText(getResources().getString(
+				R.string.confirmation));
+		mSleepTitleview.setLiftBtnVisibility(true);
+		mVisibilityProgressbarLinearLayout
+				.setVisibility(View.INVISIBLE);
+		mVisibilitySelectorTimeLinearLayout.setVisibility(View.VISIBLE);
+		if (mCount != null)
+		{
+			mCount.cancel();
+		}
+		mShowTimeTextView.setText("88:88");
 	}
 
 	@Override
@@ -158,21 +202,23 @@ public class SleepAssistantActivity extends BaseActivity implements
 		mShowTimeTextView.setText(FormatHelper
 				.formatLongToTimeMinuteStr((long) time * 60 * 1000));
 	}
-	
+
 	private void setSleepSound(long millis)
 	{
-		int cl = (time*60)/TIME_GAP;
-		for(int i =1 ;i <=TIME_GAP ; i++)
+		int cl = (time * 60) / TIME_GAP;
+		for (int i = 1; i <= TIME_GAP; i++)
 		{
-			if((millis == cl*i))
+			if ((millis == cl * i))
 			{
-				flog.d("音量为：" + (current/TIME_GAP)*i + " i: " + i + " current: " + current);
-				//如果分段的段数超过了音量最大可以加上这个，最大音量为15
-				if(current < TIME_GAP && TIME_GAP < max)
-				 {
-					 current =  TIME_GAP;
-				 }
-				mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (current/TIME_GAP)*i, 0);
+				flog.d("音量为：" + (current / TIME_GAP) * i + " i: " + i
+						+ " current: " + current);
+				// 如果分段的段数超过了音量最大可以加上这个，最大音量为15
+				if (current < TIME_GAP && TIME_GAP < max)
+				{
+					current = TIME_GAP;
+				}
+				mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+						(current / TIME_GAP) * i, 0);
 			}
 		}
 	}
@@ -187,22 +233,41 @@ public class SleepAssistantActivity extends BaseActivity implements
 		@Override
 		public void onFinish()
 		{
-			mCount.cancel();
-			mShowTimeTextView.setText("88:88");
-			mSleepTitleview.setLiftBtnVisibility(true);
+			cancelSleep();
+			playerManager.pause();
 		}
 
 		@Override
 		public void onTick(long millisUntilFinished)
 		{
 			long millis = millisUntilFinished / 1000;
-			double x = (time*60-(int) millis)/1.0;//预留小数点
-			mProgressBar.setProgress((int)((x/(time*60))*100));
+			double x = (time * 60 - (int) millis) / 1.0;// 预留小数点
+			mProgressBar.setProgress((int) ((x / (time * 60)) * 100));
 			mShowTimeTextView.setText(FormatHelper
 					.formatLongToTimeMinuteStr(millisUntilFinished));
-			 setSleepSound(millis);
+			setSleepSound(millis);
 		}
 
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		return true;
+	}
+
+	// 屏蔽返回键
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event)
+	{
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+			if(mSleepMode)
+			{
+				return true;
+			}
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 
 }
