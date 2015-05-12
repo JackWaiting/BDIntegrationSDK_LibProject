3c8e137ec5ae49396ba98c2d4a5cc460d4e14579
fiskz <root@fiskz.com>
2015-05-12 15:26:08 +0800

    蓝牙重命名字数限制10个字符

--- src/com/chipsguide/app/colorbluetoothlamp/v2/application/CustomApplication.java
+++ src/com/chipsguide/app/colorbluetoothlamp/v2/application/CustomApplication.java
@@ -10,7 +10,8 @@ public class CustomApplication extends Application {
 	/**
 	 * 蓝牙地址过滤
 	 */
-	public static final String MAC_ADDRESS_FILTER_PREFIX = "C9:7";
+//	public static final String MAC_ADDRESS_FILTER_PREFIX = "C9:7";
+	public static final String MAC_ADDRESS_FILTER_PREFIX = "";
 	/**
 	 * 蓝牙设备管理类
 	 */
@@ -25,7 +26,7 @@ public class CustomApplication extends Application {
 	public void onCreate()
 	{
 		super.onCreate();
-		CrashHandler.getInstance().init(this);
+//		CrashHandler.getInstance().init(this);
 	}
 
 	/**
