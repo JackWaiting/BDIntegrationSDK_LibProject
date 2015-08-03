8245e8917d4daf2791febe25e2c3c7a61c9604a8
fiskz <root@fiskz.com>
2015-08-03 16:17:29 +0800

    替换中文界面

--- src/com/chipsguide/app/colorbluetoothlamp/v2/application/CustomApplication.java
+++ src/com/chipsguide/app/colorbluetoothlamp/v2/application/CustomApplication.java
@@ -13,8 +13,7 @@ public class CustomApplication extends Application {
 	/**
 	 * 蓝牙地址过滤C9:7
 	 */
-//	public static final String MAC_ADDRESS_FILTER_PREFIX = "C9:7";
-	public static final String MAC_ADDRESS_FILTER_PREFIX = "";
+	public static final String MAC_ADDRESS_FILTER_PREFIX = "C9:7";
 	/**
 	 * 蓝牙设备管理类
 	 */
