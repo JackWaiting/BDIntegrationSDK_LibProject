9805595feaf4f30bd93859b090ec8b557e9e8f5d
zhanggang <zhanggang@snaillove.com>
2015-11-11 14:17:57 +0800

    MAC地址修改

--- src/com/chipsguide/app/colorbluetoothlamp/v2/brunton/application/CustomApplication.java
+++ src/com/chipsguide/app/colorbluetoothlamp/v2/brunton/application/CustomApplication.java
@@ -13,7 +13,7 @@ public class CustomApplication extends Application {
 	/**
 	 * 蓝牙地址过滤C9:7
 	 */
-	public static final String MAC_ADDRESS_FILTER_PREFIX = "C9:7";  //C9：70：08
+	public static final String MAC_ADDRESS_FILTER_PREFIX = "C9:70:08";  //C9：70：08
 	/**
 	 * 蓝牙设备管理类
 	 */
